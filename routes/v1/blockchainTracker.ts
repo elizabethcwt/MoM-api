2f7963cfe3d8a35db5b29d4c788752296e872fef
dojh111 <dojh111@gmail.com>
2022-01-08 11:47:31 +0800

    Update contract

--- routes/v1/blockchainTracker.ts
+++ routes/v1/blockchainTracker.ts
@@ -22,7 +22,7 @@ export default class BlockchainTracker {
         this.provider = new ethers.providers.JsonRpcProvider(rinkebyUrl);
         // Create Contracts
         const marketContractAddress =
-            '0xa10f5D5d7F3D5b13A66ecB831991148589459D16';
+            '0xE5B885Cb0d8dBF8B69f33CcB3BC4774a47eCd2e7';
         this.marketContract = new ethers.Contract(
             marketContractAddress,
             Market.abi,
